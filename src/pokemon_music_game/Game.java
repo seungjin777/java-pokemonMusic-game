3d33429d9ce11f9059ae267e84a7eff1a3dc3f23
seungjin777 <ds020309@naver.com>
2025-01-18 21:01:54 +0900

    devel ver-13

--- src/pokemon_music_game/Game.java
+++ src/pokemon_music_game/Game.java
@@ -5,12 +5,12 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
 public class Game extends Thread {
 	//게임 진행 관련 이미지
-	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage(); //노트 이미지
 	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage(); //노트 경로 경계선 이미지
 	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage(); //노트 판정 바
 	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage(); //게임 정보 이미지
@@ -31,15 +31,20 @@ public class Game extends Thread {
 	private String musicTitle; //곡 제목
 	private Music gameMusic; //곡 담을 변수
 	
+	//노트 관리 배열
+	ArrayList<Note> noteList = new ArrayList<Note>();
+	
 	public Game(String titleName, String difficulty, String musicTitle) {
 		this.titleName = titleName;
 		this.difficulty = difficulty;
 		this.musicTitle = musicTitle;
 		gameMusic = new Music(this.musicTitle, false);
 		gameMusic.start();
+		dropNotes(titleName);
 	}
 	
 	public void screenDraw(Graphics2D g) {
+		
 		g.drawImage(noteRouteSImage, 228, 30, null);
 		g.drawImage(noteRouteDImage, 332, 30, null);
 		g.drawImage(noteRouteFImage, 436, 30, null);
@@ -49,6 +54,13 @@ public class Game extends Thread {
 		g.drawImage(noteRouteKImage, 848, 30, null);
 		g.drawImage(noteRouteLImage, 952, 30, null);
 		
+		//각 노트들의 위치 그려주기
+		for(int i = 0; i < noteList.size(); i++) {
+			Note note = noteList.get(i);
+			note.screenDraw(g);
+		}
+		
+		//노트 판정 라인 그려기
 		g.drawImage(noteRouteLineImage, 224, 30, null);
 		g.drawImage(noteRouteLineImage, 328, 30, null);
 		g.drawImage(noteRouteLineImage, 432, 30, null);
@@ -149,5 +161,12 @@ public class Game extends Thread {
 	public void close() {
 		gameMusic.close();
 		this.interrupt();
+	}
+	
+	public void dropNotes(String titleName) {
+		//예시
+		Note note = new Note(228, "short");
+		note.start();
+		noteList.add(note);
 	}
 }
--- src/pokemon_music_game/Main.java
+++ src/pokemon_music_game/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	//해상도
 	public static final int SCREEN_WIDTH = 1280; //창 가로크기
 	public static final int SCREEN_HEIGHT = 720; //창 세로크기
+	public static final int NOTE_SPEED = 7; //노트 떨어지는 속도
+	public static final int SLEEP_TIME = 10; //노트 떨어지는 주기
 	
 	public static void main(String[] args) {
 		new DynamicBeat();
